0385ad4d661c8f00dbede22925bb5cbe03febc8c
Tran Van Tuan (Kai) <tuantran280504@gmail.com>
2025-09-26 23:22:14 +0700

    xss

--- sources/php-training/form_user.php
+++ sources/php-training/form_user.php
@@ -8,19 +8,31 @@ $user = NULL; //Add new user
 $_id = NULL;
 
 if (!empty($_GET['id'])) {
-    $_id = $_GET['id'];
-    $user = $userModel->findUserById($_id);//Update existing user
+    $_id = filter_var($_GET['id'], FILTER_VALIDATE_INT);
+    if ($_id === false) {
+        $_id = NULL;
+    } else {
+        $user = $userModel->findUserById($_id);//Update existing user
+    }
 }
 
 
 if (!empty($_POST['submit'])) {
-
-    if (!empty($_id)) {
-        $userModel->updateUser($_POST);
+    // Validate input
+    $name = trim($_POST['name'] ?? '');
+    $password = $_POST['password'] ?? '';
+    
+    if (empty($name) || empty($password)) {
+        $error = "Tên và mật khẩu không được để trống";
     } else {
-        $userModel->insertUser($_POST);
+        if (!empty($_id)) {
+            $userModel->updateUser($_POST);
+        } else {
+            $userModel->insertUser($_POST);
+        }
+        header('location: list_users.php');
+        exit;
     }
-    header('location: list_users.php');
 }
 
 ?>
@@ -38,11 +50,16 @@ if (!empty($_POST['submit'])) {
                 <div class="alert alert-warning" role="alert">
                     User form
                 </div>
+                <?php if (isset($error)) { ?>
+                    <div class="alert alert-danger" role="alert">
+                        <?php echo htmlspecialchars($error, ENT_QUOTES, 'UTF-8') ?>
+                    </div>
+                <?php } ?>
                 <form method="POST">
-                    <input type="hidden" name="id" value="<?php echo $_id ?>">
+                    <input type="hidden" name="id" value="<?php echo htmlspecialchars($_id, ENT_QUOTES, 'UTF-8') ?>">
                     <div class="form-group">
                         <label for="name">Name</label>
-                        <input class="form-control" name="name" placeholder="Name" value='<?php if (!empty($user[0]['name'])) echo $user[0]['name'] ?>'>
+                        <input class="form-control" name="name" placeholder="Name" value='<?php if (!empty($user[0]['name'])) echo htmlspecialchars($user[0]['name'], ENT_QUOTES, 'UTF-8') ?>'>
                     </div>
                     <div class="form-group">
                         <label for="password">Password</label>
